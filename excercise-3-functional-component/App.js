fa16f1d9855848a6c67d1af90fc9dee2c7361bf7
mohammad ibrar <m.abrar2018@gmail.com>
2019-02-03 01:58:24 +0500

    first commit

---
+++ excercise-3-functional-component/App.js
@@ -0,0 +1,24 @@
+import react from 'react '
+
+class Helloworld extends compoent {
+
+    render()
+    {
+    return(
+        <h1> Mohammad ibrar  </h1>
+
+    )
+    }
+}
+
+
+// stateless funcation 
+function  hello()
+{
+    <h1>Mohammad usman  </h1>
+
+}
+
+
+
+export default Helloworld
---
+++ excercise-3-functional-component/instructions.md
@@ -0,0 +1,4 @@
+# Instructions
+
+Change the Components from the assignment you made from class components to functional stateless components
+
